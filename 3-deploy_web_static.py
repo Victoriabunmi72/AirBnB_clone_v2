816eea885ebcc78968cb14333e09308a6849edbb
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-12 14:45:37 +0100

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 2-do_deploy_web_static.py)
-"""
+# web deploy pack
 
 from datetime import datetime
 from fabric.api import *
@@ -37,14 +36,14 @@ def do_deploy(archive_path):
         run("rm -rf {}/web_static".format(folder))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder))
-
         return True
     except:
         return False
 
-    def deploy():
-        """ Deploy archive! """
-        path = do_pack()
-        if path is None:
-            return False
-        return do_deploy(path)
+
+def deploy():
+    """ Deploy archive! """
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(path)
