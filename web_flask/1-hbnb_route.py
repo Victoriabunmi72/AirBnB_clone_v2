b840eec01283c69195dd643b28fcda34eab05c06
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-18 02:29:29 +0100

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,17 +3,21 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnh():
-    """This displays the str Hello HBNB!"""
+def hello_hbnb():
+    '''Displays str Hello HBNB!'''
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """This displays HBNB on path /hbnb"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
