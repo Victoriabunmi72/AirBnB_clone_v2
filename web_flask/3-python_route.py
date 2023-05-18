bd5c91657424892e6d30ebbac90ec7f0e3a5f154
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-18 02:47:58 +0100

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Your web application must be listening on 0.0.0.0, port 5000"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """This displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB on path /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays C as well as text on path"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Displays python is cool as default or text on path"""
+    text = text.replace('_', ' ')
+    return "Python  {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
