1d62fbc4b701e905c01367e0d115fa61bc0af1ce
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-21 07:45:10 +0100

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python3
-'''
-Starts Flask web application
-'''
-from flask import Flask
+"""
+A python script that starts a Flask web application
+"""
 
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    '''Displays str Hello HBNB!'''
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Display a greeting message
+    """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    ''' Displays HBNB on path /hbnb'''
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_():
+    """
+    Display a message
+    """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    ''' Displays C as well as text on path '''
-    text = text.replace('_', ' ')
-    return "C {}".format(text)
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is cool"):
-    '''Displays python is cool as default or text on path '''
-    text = text.replace('_', ' ')
-    return "Python  {}".format(text)
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text=None):
+    """
+    Display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    if text is not None:
+        return "Python {}".format(text.replace("_", " "))
+    return "Python is cool"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
